3a034b824b966faed425e74cb468c31951d619e3
jorgeCasalini <joshcasalini@gmail.com>
2025-05-25 15:39:16 -0300

    Adds new PostgreSQL skill text

--- src/components/_TextAnimationSkills.jsx
+++ src/components/_TextAnimationSkills.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
 const TextAnimationSkills = () => {
-    return(
-        <div className="overflow-hidden whitespace-nowrap mb-16" data-aos="fade-left" data-aos-duration="3000">
-          <div className="inline-block animate-scroll">
-            <span className="text-2xl md:text-4xl font-bold mx-4 encode-sans-semi-condensed-bold">
-              React • Node.js • HTML • CSS • Git • GitHub • JavaScript • Bootstrap • Tailwind • Figma •
-            </span>
-            <span className="text-2xl md:text-4xl font-bold mx-4 encode-sans-semi-condensed-bold">
-              React • Node.js • HTML • CSS • Git • GitHub • JavaScript • Bootstrap • Tailwind • Figma •
-            </span>
-          </div>
-        </div>
-    );
+  return (
+    <div
+      className="overflow-hidden whitespace-nowrap mb-16"
+      data-aos="fade-left"
+      data-aos-duration="3000"
+    >
+      <div className="inline-block animate-scroll">
+        <span className="text-2xl md:text-4xl font-bold mx-4 encode-sans-semi-condensed-bold">
+          React • Node.js • HTML • CSS • JavaScript • GitHub • Git • Tailwind •
+          Bootstrap • PostgreSQL • Figma •
+        </span>
+        <span className="text-2xl md:text-4xl font-bold mx-4 encode-sans-semi-condensed-bold">
+          React • Node.js • HTML • CSS • JavaScript • GitHub • Git • Tailwind •
+          Bootstrap • PostgreSQL • Figma •
+        </span>
+      </div>
+    </div>
+  );
 };
 
-export default TextAnimationSkills
+export default TextAnimationSkills;
